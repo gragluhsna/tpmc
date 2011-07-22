ce740b669ec5052cc2fc415510e6298a780049dc
Anshul Garg <anshul.garg@gmail.com>
2011-07-22 18:36:53 +0800

    Changed producer lots view

--- app/helpers/producer_lots_helper.rb
+++ app/helpers/producer_lots_helper.rb
@@ -1,7 +1,14 @@
 module ProducerLotsHelper
 
-	def producers_for_select
-		Producer.find(:all, :order => 'name').collect {|f| [ f.name, f.id ] }
+	def producer_lot_details_collated(producer_lot)
+		#@producer_lot_details = producer_lot.producer_lot_details.joins(:product).order("products.code")
+		@producer_lot_details = Array.new
+		Product.find(:all).each do |product|
+			@total_quantity = ProducerLotDetail.sum(:quantity, :conditions => ["product_id = " + product.id.to_s , "producer_lot_id = " + producer_lot.id.to_s])
+			@producer_lot_details << ProducerLotDetail.new 
+			#{ producer_lot_id => producer_lot.id, product_id => product.id, quantity => @total_quantity }
+		end 
+		return @producer_lot_details
 	end
 
 end
