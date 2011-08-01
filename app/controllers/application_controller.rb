8eb4c1a4db93fe298856c364a54c228096f6cf78
Anshul Garg <anshul.garg@gmail.com>
2011-08-01 17:40:34 +0800

    Trying ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :correct_safari_and_ie_accept_headers
+ 
+  def correct_safari_and_ie_accept_headers
+    request.accepts.sort!{ |x, y| y.to_s == 'application/xml' ? 1 : -1 } if request.xhr?
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// All ajax requests will trigger the format.xml block
+// of +respond_to do |format|+ declarations
+$.ajaxSetup({
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/xml")}
+})
